7075131481b6b2b1a1d4261487c2ebad8f8a7670
vinicius-mattoso <vinicius.vmrs@gmail.com>
2023-01-22 21:59:32 -0300

    [updade] 01.py entrada de dados

---
+++ Aula_entrada_de_dados/01.py
@@ -0,0 +1,28 @@
+######################################
+# Entrada de dados
+# Aula 07 das aulas da FEI de interface gráficas
+# Vinicius Mattoso
+######################################
+
+# O tradicional input que utilizamos em python vai ser o novo Entry.
+
+# input -> CLI console Application
+# entry -> GUI Graphical Application
+
+# OBS: O formato tradicional de entrada e o string
+
+# var = input()
+
+# self.entrada = tk.Entry (WidgetPai, opõesDeConfiguração)
+# self.qualquer = float( self.entrada.get() )
+
+# não podemos utilizar a opção de height para controlar a altura da entrada, pois a mesma vai ser sempre de uma linha!
+
+#bonus:
+# a opção show='*' dentro do .Entry, faz com que tudo que o usuário digitar fique escondido por *
+
+######################################
+# O widget Button
+# Aula 08 das aulas da FEI de interface gráficas
+# Vinicius Mattoso
+######################################
