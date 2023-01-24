84118dc73adab2b4e86d8dea117ee59a584afce2
vinicius-mattoso <vinicius.vmrs@gmail.com>
2023-01-23 22:27:50 -0300

    [add] aulas entrada de dados

--- Aula_entrada_de_dados/01.py
+++ Aula_entrada_de_dados/01.py
@@ -21,8 +21,30 @@
 #bonus:
 # a opção show='*' dentro do .Entry, faz com que tudo que o usuário digitar fique escondido por *
 
-######################################
-# O widget Button
-# Aula 08 das aulas da FEI de interface gráficas
-# Vinicius Mattoso
-######################################
+import tkinter as tk
+
+
+class Tela:
+    def __init__(self, master):
+
+        self.nossaTela = master
+
+        #adicionando um texto a nossa interface
+        self.texto1 = tk.Label(self.nossaTela, text="Por gentilize,\n insira seu nome:")
+        self.texto1.pack(side=tk.LEFT, fill=tk.X)
+
+        #adicionando um widget de entrada de dados
+        self.entrada = tk.Entry(self.nossaTela)
+        self.entrada.pack(side=tk.LEFT, padx=10)
+
+        #adicionando um widget de botão já pronto para servir como aceite do nome 
+        self.btn = tk.Button(self.nossaTela, text="Confirmar",bg='green', command=self.mostraNome)
+        self.btn.pack(side=tk.LEFT, padx=10)
+
+    def mostraNome(self):
+        self.nome = self.entrada.get()
+        print('Olá:' + self.nome + " seja muito bem vindo a minha primeira interface")
+
+janelaRaiz = tk.Tk()
+Tela(janelaRaiz)
+janelaRaiz.mainloop()
